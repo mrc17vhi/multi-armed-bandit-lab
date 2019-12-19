2765f1daf00240645803d8038e3d02ebf1137e51
mrc17vhi <58977651+mrc17vhi@users.noreply.github.com>
2019-12-19 22:52:20 +0100

    Resub mrc17vhi-mrc17ksm resub try 3

--- test/test_runner_mrc17vhi-mrc17ksm.py
+++ test/test_runner_mrc17vhi-mrc17ksm.py
@@ -1,12 +1,10 @@
-
+import bandit
 import os,sys,inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 sys.path.insert(0, os.path.dirname(parent_dir))
 
-
-import bandit
 
 def simulate():
     results = []
